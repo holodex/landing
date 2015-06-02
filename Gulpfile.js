f2f7cf77aa93e02c1e4639a35df9abd6d0a6ba69
Michael Williams <dinosaur@riseup.net>
2015-06-03 00:03:16 +1200

    move props to separate file in ./src

--- Gulpfile.js
+++ Gulpfile.js
@@ -10,19 +10,8 @@ var nodeEnv = env.NODE_ENV
 
 var lr
 
-var props = {
-  name: "Holodex",
-  pitchPhrase: "People and Groups",
-  pitchSentence: "Holodex empowers you to find people, understand relationships, and manage roles in your groups.",
-  description: "Holodex is a web-based directory to search, visualize, and organize networks of people. The system is built around you, so everyone can come together.",
-  valueProps: [],
-  callToAction: {
-    text: "Request an invite",
-    url: ""
-  }
-}
-
 function html () {
+  var props = require('./src/props')
   return gulp.src('src/views/*.js', { read: false })
     .pipe(plumber())
     .pipe(render('markup', props))
