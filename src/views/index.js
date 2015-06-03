ab1ddbf79429bcf5176ae9d702ef639b379660c3
Michael Williams <dinosaur@riseup.net>
2015-06-04 00:30:13 +1200

    continue to flush out landing page content and styles

--- src/views/index.js
+++ src/views/index.js
@@ -20,19 +20,26 @@ var style = Stylesheet.create({
     padding: vars.gutterWidth
   },
   introBlock: {
-    backgroundImage: 'linear-gradient(top, #6f27ff, rgba(0,0,0,0));'
+    backgroundColor: '#6f27ff',
+    //backgroundImage: '-webkit-linear-gradient(top, #6f27ff, rgba(0,0,0,0));',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
   },
   introH1: {
-    fontSize: vars.fontBase * 3,
+    marginBottom: 0,
+    fontSize: vars.fontBase * 3.8,
     fontWeight: 700,
-    lineHeight: 1.1
+    lineHeight: 1.1,
+    color: 'white'
   },
   introH2: {
     fontSize: vars.fontBase * 2,
     fontWeight: 300,
-    lineHeight: 1.1
+    lineHeight: 1.1,
+    width: '75%'
   },
-  about: {
+  aboutBlock: {
 
   },
   aboutH1: {
@@ -41,16 +48,33 @@ var style = Stylesheet.create({
   aboutP: {
 
   },
-  value: {
+  aboutCarousel: {
 
   },
-  who: {
+  whoBlock: {
+
+  },
+  actionBlock: {
 
   },
   actionButton: {
+    height: vars.fontBase * 4,
+    width: vars.fontBase * 24,
+    fontSize: vars.fontBase * 1.6,
+    border: '2px solid black',
+    borderRadius: 10,
+    color: 'black',
     outline: 0,
+    fontWeight: 300
+  },
+  button: {
     'WebkitAppearance': 'button',
-    cursor: 'pointer'
+    cursor: 'pointer',
+    alignItems: 'flex-start',
+    textAlign: 'center',
+    color: 'buttontext',
+    backgroundColor: 'buttonface',
+    textDecoration: 'none'
   }
 })
 
@@ -74,10 +98,31 @@ module.exports = React.createClass({
           r.a({
             className: style.actionButton,
             href: props.callToAction.url
-          }, props.callToAction.text)
+          }, [
+            r.button({
+              className: style.button + ' ' + style.actionButton,
+            }, props.callToAction.text)
+          ])
         ]),
-        r.section({}, [
-          
+        r.section({
+          className: style.block + ' ' + style.aboutBlock
+        }, [
+          r.h1({
+            className: style.aboutH1
+          }, "What is " + props.name + "?"),
+          r.p({
+            className: style.aboutP
+          }, props.description)
+        ]),
+        r.section({
+          className: style.block + ' ' + style.whoBlock
+        }),
+        r.section({
+          className: style.block + ' ' + style.actionBlock
+        }, [
+          r.p({
+            className: style.actionP
+          }, props.callToAction.text)
         ])
       ])
     ])
