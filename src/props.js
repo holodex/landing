974aa4aece460f30818be1276067d7c62a5364fc
Michael Williams <dinosaur@riseup.net>
2015-06-04 16:45:54 +1200

    minor updates to pitch and description props

--- src/props.js
+++ src/props.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: "Holodex",
   pitchPhrase: "People and Groups",
-  pitchSentence: "Holodex empowers you to find people, understand relationships, and manage roles in your groups.",
-  description: "Holodex is a web-based directory to search, visualize, and organize networks of people. The system is built around you, so everyone can come together.",
+  pitchSentence: "Holodex empowers you to discover people, understand relationships, and organize roles as they emerge in your groups.",
+  description: "Holodex is a web-based directory to search, visualize, and manage groups of people. The system is built around you, so everyone can come together.",
   valueProps: [],
   callToAction: {
     text: "Request an invite",
